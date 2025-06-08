063e53c298f26c7a011962dfc2af7f59e15c0301
LeNguyenKhanhDuy <n22dcat012@student.ptithcm.edu.vn>
2025-06-08 08:40:51 +0000

    initial commit from codespaces

--- met_film/settings.py
+++ met_film/settings.py
@@ -9,31 +9,32 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#Emain password recover
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+# Emain password recover
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'n22dcat047@student.ptithcm.edu.vn'
-EMAIL_HOST_PASSWORD = 'hpre czgr izdj xbqt'  # Không dùng password thường, phải là App Password
+EMAIL_HOST_USER = "n22dcat047@student.ptithcm.edu.vn"
+EMAIL_HOST_PASSWORD = (
+    "hpre czgr izdj xbqt"  # Không dùng password thường, phải là App Password
+)
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r$@(mez00+(supnyu%jw0axtf_k)!vx163wk8hrymz2d+^=^3o'
+SECRET_KEY = "django-insecure-r$@(mez00+(supnyu%jw0axtf_k)!vx163wk8hrymz2d+^=^3o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,55 +45,55 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin', #giao dien admin custom, phai dat tren cung neu khong dung admin default
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'movies',
-    'user_interactions',
-    'accounts',
-    'actor',
-    'payment',
-    'widget_tweaks',
+    "jazzmin",  # giao dien admin custom, phai dat tren cung neu khong dung admin default
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.humanize",
+    "movies",
+    "user_interactions",
+    "accounts",
+    "actor",
+    "payment",
+    "widget_tweaks",
 ]
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'met_film.urls'
+ROOT_URLCONF = "met_film.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
-                'payment.context_processors.user_subscription_status',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
+                "payment.context_processors.user_subscription_status",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'met_film.wsgi.application'
+WSGI_APPLICATION = "met_film.wsgi.application"
 
 
 # Database
@@ -103,13 +104,13 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'film_database',
-        'USER': 'root',
-        'PASSWORD': 'thantam@311',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "film_database",
+        "USER": "root",
+        "PASSWORD": "my-secret-pw",
+        "HOST": "127.0.0.1",
+        "PORT": "3307",
     }
 }
 
@@ -119,16 +120,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -136,9 +137,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'vi'
+LANGUAGE_CODE = "vi"
 
-TIME_ZONE = 'Asia/Ho_Chi_Minh'
+TIME_ZONE = "Asia/Ho_Chi_Minh"
 
 USE_I18N = True
 
@@ -150,27 +151,29 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = 'home' 
+LOGIN_REDIRECT_URL = "home"
 
 # STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
 # VNPAY CONFIG
-VNPAY_RETURN_URL = 'http://localhost:8000/payment/vnpay-return/'  # get from config
-VNPAY_PAYMENT_URL = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'  # get from config
-VNPAY_API_URL = 'https://sandbox.vnpayment.vn/merchant_webapi/api/transaction'
-VNPAY_TMN_CODE = 'ZR78PY19'  # Website ID in VNPAY System, get from config
-VNPAY_HASH_SECRET_KEY = 'PXM5EVOBYQRF601LXCYLWYYPMRWZGQ7H'  # Secret key for create checksum,get from config
+VNPAY_RETURN_URL = "http://localhost:8000/payment/vnpay-return/"  # get from config
+VNPAY_PAYMENT_URL = (
+    "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"  # get from config
+)
+VNPAY_API_URL = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction"
+VNPAY_TMN_CODE = "ZR78PY19"  # Website ID in VNPAY System, get from config
+VNPAY_HASH_SECRET_KEY = (
+    "PXM5EVOBYQRF601LXCYLWYYPMRWZGQ7H"  # Secret key for create checksum,get from config
+)
